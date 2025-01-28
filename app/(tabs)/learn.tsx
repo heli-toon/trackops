a68ee2a7229e0c42bbb01368145b392ce8e2657b
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-28 16:16:37 +0000

    Initial Commit

---
+++ app/(tabs)/learn.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function learn() {
+  return (
+    <View>
+      <Text>learn</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
